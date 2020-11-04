f95950996d8e3be3cf3f2d8336b0860ed569ce4a
Andrzej.G <gatkowski.a@gmail.com>
2020-11-04 21:29:53 +0100

    CHANGE routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import { Dashboard } from 'src/components/dashboard';
 import { Login } from 'src/components/login';
 import { SignUp } from 'src/components/signup';
+import { PageNotFound } from 'src/pages/404';
+import { ApplicationState } from 'src/store/reducers';
 
 export const Routes = () => {
-  const isAuthorised = true;
+  const isAuthorised = useSelector((state: ApplicationState) => state.auth.isLoggedIn);
 
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => (isAuthorised ? <Redirect to="/dashboard" /> : <Redirect to="/login" />)} />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            if (isAuthorised) {
+              return <Redirect to="/dashboard" />;
+            } else {
+              return <Redirect to="/login" />;
+            }
+          }}
+        />
         {isAuthorised && <Route exact path="/dashboard" component={Dashboard} />}
-        {/*<AuthorizationRoutes />*/}
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={SignUp} />
-        {/*<DashboardRoutes />*/}
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={SignUp} />
+        <Route path="*" component={PageNotFound} />
       </Switch>
     </Router>
   );
