907ccc84ccb301c242047d6b2b5ad68a0742c7f3
Andrzej.G <gatkowski.a@gmail.com>
2020-11-04 21:25:42 +0100

    ADD translations

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
